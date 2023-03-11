f0e779f849dcd2b4fa774c705daca8980b07cf3c
Rodrigo Lacerda <rodrigo.lacerda@rappi.com>
2023-03-11 19:46:40 -0300

    decrease limit to 200

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Card from '../components/Card'
 
 export async function getStaticProps(){
 
-  const maxPokemons = 300
+  const maxPokemons = 200
 
   const api = 'https://pokeapi.co/api/v2/pokemon/'
 
