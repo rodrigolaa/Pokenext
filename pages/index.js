d0f23455cef14ae609c044064dced9acb77e9ad6
Rodrigo Lacerda <rodrigo.lacerda@rappi.com>
2023-03-12 10:59:16 -0300

    removed api fetch in index

--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,78 @@ import {useRouter} from 'next/router'
 
 export async function getStaticProps( ){
 
-  const maxPokemons = 251
+  // const maxPokemons = 10
 
-  const api = 'https://pokeapi.co/api/v2/pokemon/'
+  // const api = 'https://pokeapi.co/api/v2/pokemon/'
 
-  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  // const res = await fetch(`${api}/?limit=${maxPokemons}`)
 
-  const data = await res.json()
+  // const data = await res.json()
 
-  //add pokemon index
+  // //add pokemon index
 
 
-  data.results.forEach((item,index)=> {
-    item.id = index + 1
+  // data.results.forEach((item,index)=> {
+  //   item.id = index + 1
 
-  })
+  // })
+
+  // console.log(data.results)
 
   return {
     props: {
-      pokemons: data.results,
+      pokemons: [
+  {
+    name: 'bulbasaur',
+    url: 'https://pokeapi.co/api/v2/pokemon/1/',
+    id: 1
+  },
+  {
+    name: 'ivysaur',
+    url: 'https://pokeapi.co/api/v2/pokemon/2/',
+    id: 2
+  },
+  {
+    name: 'venusaur',
+    url: 'https://pokeapi.co/api/v2/pokemon/3/',
+    id: 3
+  },
+  {
+    name: 'charmander',
+    url: 'https://pokeapi.co/api/v2/pokemon/4/',
+    id: 4
+  },
+  {
+    name: 'charmeleon',
+    url: 'https://pokeapi.co/api/v2/pokemon/5/',
+    id: 5
+  },
+  {
+    name: 'charizard',
+    url: 'https://pokeapi.co/api/v2/pokemon/6/',
+    id: 6
+  },
+  {
+    name: 'squirtle',
+    url: 'https://pokeapi.co/api/v2/pokemon/7/',
+    id: 7
+  },
+  {
+    name: 'wartortle',
+    url: 'https://pokeapi.co/api/v2/pokemon/8/',
+    id: 8
+  },
+  {
+    name: 'blastoise',
+    url: 'https://pokeapi.co/api/v2/pokemon/9/',
+    id: 9
+  },
+  {
+    name: 'caterpie',
+    url: 'https://pokeapi.co/api/v2/pokemon/10/',
+    id: 10
+  }
+],
     }
   }
 
